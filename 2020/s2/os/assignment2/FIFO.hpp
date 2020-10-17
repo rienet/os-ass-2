a302800a70ec469a1882763b1ff9abdbc029aea1
a1749955 <a1749955@c7dd522b-9508-4d02-9294-8ba919d5c64a>
2020-10-17 08:06:45 +0000

    heh

--- 2020/s2/os/assignment2/FIFO.hpp
+++ 2020/s2/os/assignment2/FIFO.hpp
@@ -1,9 +1,15 @@
 #include "frame.h"
+#include "page.h"
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-void FIFO(){
-    cout << "pog" << endl;
+void FIFO(vector<Page> page_table, vector<Frame> frame_table, vector<Frame> memory_traces){
+    // go thru the entire memory trace
+    for(int time = 0; time<memory_traces.size(); time++){
+
+        //
+        int current_page_num = memory_traces[time].page_number;
+    }
 }
